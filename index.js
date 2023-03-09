f1ba5d954f03e36e64d559b7bcc9e3c74ce19865
Risan Ahamed <risanzahamed76@gmail.com>
2023-03-09 20:15:06 +0600

    add

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+
+const stripe = require("stripe")("sk_test_51M5vi0CWY41z4WgMZuFcRTVhB7lmsHo7mVZ4HyvVpn7tCZIkG8M0XlbpFpGIZdNmc1JEMAzUz6HqEAAdaxwetoFL00kgJNeblU");
+
 
 // const jwt = require('jsonwebtoken');
 const app = express()
@@ -16,22 +19,7 @@ app.get('/', (req, res) => {
     res.send('Kick Scooter and Bicycle server is running')
 })
 
-function jwtVerify(req, res, next) {
-    const authHeader = req.headers.authorization
 
-    if (!authHeader) {
-        return res.status(401).send('Unauthorized Access data not found');
-    }
-    const token = authHeader.split(' ')[1]
-    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-        if (err) {
-            return res.status(403).send({ message: 'forbidden access' })
-        }
-        req.decoded = decoded
-        next()
-    })
-
-}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h3zxwhp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -48,16 +36,16 @@ async function run() {
         const bestSellingProduct= client.db('rentRoll').collection('bestSellingProduct')
         // const carDetailsByCategory = client.db('rentRoll').collection('ourcardd')
         const productAddToCartCollection = client.db('rentRoll').collection('productCart')
-        // const carflagedCollection = client.db('rentRoll').collection('flagedItems')
+        const productflagedCollection = client.db('rentRoll').collection('flagedItems')
         const usersCollection = client.db('rentRoll').collection('users')
         // const sellerCollection = client.db('rentRoll').collection('seller')
         // const customerCollection = client.db('rentRoll').collection('customer')
-        // const paymentCollection = client.db('rentRoll').collection('payment')
+        const paymentCollection = client.db('rentRoll').collection('payment')
 
         const addProductCollection = client.db('rentRoll').collection('addProduct')
         // const advertiseCollection = client.db('rentRoll').collection('advertiseItem')
 
-        // payment
+
 
         // app.post('/payment', async(req, res)=>{
         //     const payment = req.body
@@ -70,31 +58,23 @@ async function run() {
         //             transactionId: payment.transactionId
         //         }
         //     }
-        //     const updatedResult = await carBookingsCollection.updateOne(filter, updatedDoc)
+        //     const updatedResult = await paymentCollection.updateOne(filter, updatedDoc)
         //     res.send(result)
         // })
 
 
-        // JWT CODE
-        app.get('/jwt', async (req, res) => {
-            const email = req.query.email;
-            const query = { email: email };
-            const user = await usersCollection.findOne(query);
+        // // JWT CODE
+        // app.get('/jwt', async (req, res) => {
+        //     const email = req.query.email;
+        //     const query = { email: email };
+        //     const user = await usersCollection.findOne(query);
 
-            if (user) {
-                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '5d' })
-                return res.send({ accessToken: token })
-            }
-            res.status(403).send({ accessToken: '' })
-        })
-
-
-        // app.get('/product-category', async (req, res) => {
-        //     const query = {}
-        //     const cursor = productCategories.find(query);
-        //     const result = await cursor.toArray();
-        //     res.send(result);
-        // });
+        //     if (user) {
+        //         const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '5d' })
+        //         return res.send({ accessToken: token })
+        //     }
+        //     res.status(403).send({ accessToken: '' })
+        // })
 
         app.get('/products', async (req, res) => {
             const query = {}
@@ -149,13 +129,6 @@ async function run() {
             res.send(product)
         })
 
-        // app.get('/car-details', async (req, res) => {
-        //     const query = {}
-        //     const cursor = newArrivals.find(query);
-        //     const result = await cursor.toArray();
-        //     res.send(result);
-        // });
-
         app.get('/category/:id', async (req, res) => {
             const id = req.params.id
             const query = { categoryName: id }
@@ -193,51 +166,52 @@ async function run() {
         app.get('/booking/:id', async (req, res) => {
             const id = req.params.id
 
-            const query = { _id: ObjectId(id) }
+            const query = { _id: new ObjectId(id) }
 
             const result = await productAddToCartCollection.findOne(query)
             res.send(result)
         })
 
 
-        // app.post('/flag-items', async (req, res) => {
-        //     const flaged = req.body
+        app.post('/flag-items', async (req, res) => {
+            const flaged = req.body
 
-        //     const query = {
-        //         image: flaged.image,
-        //         model: flaged.model,
-        //         category: flaged.categoryName,
-        //         description: flaged.description,
-        //         resalePrice: flaged.resalePrice,
-        //         originalPrice: flaged.originalPrice,
-        //         postDate: flaged.postDate,
-        //         yearsOfUse: flaged.yearsOfUse,
-        //         location: flaged.location,
-        //     }
+            const query = {
+                image: flaged.image,
+                model: flaged.model,
+                category: flaged.categoryName,
+                description: flaged.description,
+                resalePrice: flaged.resalePrice,
+                originalPrice: flaged.originalPrice,
+                postDate: flaged.postDate,
+                yearsOfUse: flaged.yearsOfUse,
+                location: flaged.location,
+            }
 
-        //     const result = await carflagedCollection.insertOne(query)
-        //     res.send(result)
-        // })
+            const result = await productflagedCollection.insertOne(query)
+            res.send(result)
+        })
 
 
-        // app.get('/flag-items', async (req, res) => {
-        //     const email = req.query.email
-        //     const query = { email: email }
-        //     const carbookings = await carflagedCollection.find(query).toArray()
-        //     res.send(carbookings)
+        app.get('/flag-items', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const carbookings = await productflagedCollection.find(query).toArray()
+            res.send(carbookings)
 
-        // })
-
-        // app.get('/users', async (req, res) => {
-        //     const query = {}
-        //     const result = await usersCollection.find(query).toArray()
-        //     res.send(result)
-        // })
+        })
 
         app.post('/user', async (req, res) => {
             const user = req.body
             const result = await usersCollection.insertOne(user)
             res.send(result)
+        })
+
+        app.get('/user', async (req, res) => {
+            const query = {}
+            const cursor = usersCollection.find(query)
+            const service = await cursor.toArray()
+            res.send(service)
         })
 
         app.get("/user/:email", async (req, res) => {
@@ -251,59 +225,43 @@ async function run() {
       
             res.send({ status: false });
           });
+          app.get('/user/:id', async (req, res) => {
+            const id = req.params.id
 
-        // app.get('/users/admin/:email', async (req, res) => {
-        //     const email = req.params.email
-        //     const query = { email }
-        //     const user = await usersCollection.findOne(query)
-        //     res.send({ isAdmin: user?.role === 'admin' })
-        // })
+            const query = { role : new ObjectId(id) }
 
+            const result = await usersCollection.findOne(query)
+            res.send(result)
+        })
+          app.get('/user/:id', async (req, res) => {
+            const role = req.params.role
 
-        // app.put('/users/admin/:id', jwtVerify, async (req, res) => {
-        //     const decodedEmail = req.decoded.email
-        //     const query = { email: decodedEmail }
-        //     const user = await usersCollection.findOne(query)
+            const query = { role : new ObjectId(role) }
 
-        //     if (user.role !== 'admin') {
-        //         return res.status(403).send({ message: 'forbidden access' })
-        //     }
-
-
-        //     const id = req.params.id
-        //     const filter = { _id: ObjectId(id) }
-        //     const options = { upsert: true };
-        //     const updatedDoc = {
-        //         $set: {
-        //             role: "admin"
-        //         }
-        //     }
-        //     const result = await usersCollection.updateOne(filter, updatedDoc, options)
-        //     res.send(result)
-        // })
+            const result = await usersCollection.find(query)
+            res.send(result)
+        })
 
 
+        app.post("/create-payment-intent", async (req, res) => {
+            const productBooked = req.body;
+            const price = productBooked.price
+            const amount = price * 100
 
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: "usd",
+                "payment_method_types": [
+                    "card"
+                ],
+            });
 
-        // app.post("/create-payment-intent", async (req, res) => {
-        //     const carbooking = req.body;
-        //     const price = carbooking.price
-        //     const amount = price * 100
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
 
-        //     const paymentIntent = await stripe.paymentIntents.create({
-        //         amount: amount,
-        //         currency: "usd",
-        //         "payment_method_types": [
-        //             "card"
-        //         ],
-        //     });
-
-        //     res.send({
-        //         clientSecret: paymentIntent.client_secret,
-        //     });
-        // });
-
-
+        
 
         app.post('/add-product', async (req, res) => {
             const product = req.body
@@ -317,89 +275,6 @@ async function run() {
             const result = await addProductCollection.find(query).toArray()
             res.send(result)
         })
-
-
-
-        // app.put('/advertise', async (req, res) => {
-        //     const advertise = req.body
-
-        //     const query = {
-        //         sellerName: advertise.sellerName,
-        //         image: advertise.image,
-        //         model: advertise.model,
-        //         category: advertise.name,
-        //         description: advertise.description,
-        //         resalePrice: advertise.sellingPrice,
-        //         originalPrice: advertise.originalPrice,
-        //         postDate: advertise.postDate,
-        //         yearsOfUse: advertise.yearsOfUse,
-        //         location: advertise.location,
-        //     }
-
-        //     const result = await advertiseCollection.insertOne(query)
-        //     res.send(result)
-        // })
-
-
-        // app.get('/advertise', async (req, res) => {
-        //     const email = req.query.email
-        //     const query = { email: email }
-        //     const carbookings = await advertiseCollection.find(query).toArray()
-        //     res.send(carbookings)
-
-        // })
-
-
-        // // Seller api 
-
-        // app.post('/seller', async (req, res) => {
-        //     const user = req.body
-        //     const result = await sellerCollection.insertOne(user)
-        //     res.send(result)
-        // })
-
-
-        // app.get('/users/seller/:email', async (req, res) => {
-        //     const email = req.params.email
-        //     const query = { email }
-        //     const user = await sellerCollection.findOne(query)
-        //     res.send({ isSeller: user?.seller === 'seller' })
-        // })
-
-
-        // app.get('/user/seller', async (req, res) => {
-        //     const query = {}
-        //     const result = await sellerCollection.find(query).toArray()
-        //     res.send(result)
-        // })
-
-
-        // // Custommer/ Buyer Api
-
-        // app.post('/customer', async (req, res) => {
-        //     const user = req.body
-        //     const result = await customerCollection.insertOne(user)
-        //     res.send(result)
-        // })
-
-
-        // app.get('/users/customer/:email', async (req, res) => {
-        //     const email = req.params.email
-        //     const query = { email }
-        //     const user = await customerCollection.findOne(query)
-        //     res.send({ isCustomer: user?.customer === 'customer' })
-        // })
-
-
-        // app.get('/user/customer', async (req, res) => {
-        //     const query = {}
-        //     const result = await customerCollection.find(query).toArray()
-        //     res.send(result)
-        // })
-
-
-
-
     }
 
     finally {
